5d22eef92e8410b7a4f973fbd0ccc96f8ccae2b0
BLACKCODE1234 <emmanuelamankwah004@gmail.com>
2025-11-19 17:54:13 +0000

    signup route

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,19 @@ from psycopg2 import RealDictCursor
 
 @app.route('/signup',methods=['POST'])
 def signup():
-    if not request.is_json:
-        return jsonify({"message":"Missing Data,400"})    
-
     
+    if not request.is_json:
+
+        return jsonify({"message":"Request must be JSON","status":400})    
+
+    data=request.get_json()
+    firstname = request.get_json("firstname")
+    lastname - request.get_json("lastname")
+    email = request.get_json("email")
+    password = request.get_json("password")
+    repassword = request.get_json("repassword")
+    
+
 
 
 if __name__ == '__main__':
