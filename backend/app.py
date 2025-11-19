4769fb19ac0062f9ba2c0a1598e45fe8c740d256
BLACKCODE1234 <emmanuelamankwah004@gmail.com>
2025-11-19 17:42:54 +0000

    started the signup route

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,13 @@ from flask_cors import CORS
 from psycopg2 import RealDictCursor
 
 
+@app.route('/signup',methods=['POST'])
+def signup():
+    if not request.is_json:
+        return jsonify({"message":"Missing Data,400"})    
+
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
