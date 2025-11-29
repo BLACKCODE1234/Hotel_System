74ed7539204f5eb5b55902a5050ceeebe3119af2
BLACKCODE1234 <emmanuelamankwah004@gmail.com>
2025-11-29 18:43:37 +0000

    helper function

--- backend/helper/generate_token.py
+++ backend/helper/generate_token.py
@@ -8,6 +8,8 @@ def generate_access_token(email):
     payload ={
         'email':email,
         'role':role,
-        'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
+        'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=int(os.getenv('ACCESS_TOKEN_EXPIRES_MINUTES',15))),
         'iat':datetime.datetime.utcnow()
     }
+token = jwt.encode(payload,os.getenv('JWT_KEY'),algorithm='HS256')
+return token
