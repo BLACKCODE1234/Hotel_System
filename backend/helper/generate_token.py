847907b497c410c2568ae53728b7f096a28a07d5
BLACKCODE1234 <emmanuelamankwah004@gmail.com>
2025-11-29 18:36:28 +0000

    helper function

--- backend/helper/generate_token.py
+++ backend/helper/generate_token.py
@@ -1,3 +1,11 @@
+import email
 import jwt
 import os
 import datetime
+
+
+def generate_access_token(email):
+    payload ={
+        'email':email,
+        'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+    }
