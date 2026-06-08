33aca16d3a69072743d9d0ae1c5c9c4ae47bd215
BLACKCODE1234 <emmanuelamankwah004@gmail.com>
2026-06-08 02:49:48 +0000

    updated the auth_service file in the service folder

--- backend/service/auth_service.py
+++ backend/service/auth_service.py
@@ -9,5 +9,38 @@ from repository.user_repository import (
     user_account_check,
     get_user_by_email
 )
+from fastapi import jsonify
+from models.schemas import (UserLogin,
+    UserSignup,
+    AdminLogin,
+    StaffLogin,
+    OTPRequest,
+    OTPVerify
+)
 
-from models.schemas import 
+
+
+
+def signup(data: UserSignup):
+    existing_user = user_account_check(data.email)
+    
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    if data.password != data.confirmpassword:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+    
+    password = hash_password(data.password)
+    otp = generate_otp()
+    hashedotp = hash_otp(otp)
+    expiry = datetime.now() + timedelta(minutes=5)
+    attempts = 0
+    
+    save_pending_user(
+        data.username,
+        data.email,
+        password
+    )
+    save_otp(data.email, hashedotp, expiry, attempts)
+    send_email(data.email, otp)
+    return {"message": "Check your email for OTP."}  
