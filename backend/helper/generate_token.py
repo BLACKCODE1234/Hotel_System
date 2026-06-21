283f3fd4355601e5310421438e2e398fcaba007d
emmanuel amankwah <emmanuelamankwah004@gmail.com>
2026-06-21 16:19:15 +0000

    changes in the helper function

--- backend/helper/generate_token.py
+++ backend/helper/generate_token.py
@@ -1,35 +1,39 @@
+import email
+import secrets
 import jwt
 import os
 import datetime
 
 
-def generate_access_token(email, role='user'):
-    payload = {
-        'email': email,
-        'role': role,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=int(os.getenv('ACCESS_TOKEN_EXPIRES_MINUTES', 15))),
-        'iat': datetime.datetime.utcnow()
+def generate_access_token(email,role='user'):
+    payload ={
+        'email':email,
+        'role':role,
+        'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=int(os.getenv('ACCESS_TOKEN_EXPIRES_MINUTES',15))),
+        'iat':datetime.datetime.utcnow()
     }
-    return jwt.encode(payload, os.getenv('JWT_KEY'), algorithm='HS256')
+    token = jwt.encode(payload,os.getenv('JWT_KEY'),algorithm='HS256')
+    return token
 
 
-def generate_refresh_token(email, role='user'):
+def generate_refresh_token(email,role='user'):
     payload = {
-        'email': email,
-        'role': role,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=int(os.getenv("REFRESH_TOKEN_EXPIRES_DAYS", 7))),
-        'iat': datetime.datetime.utcnow()
+        'email':email,
+        'role':role,
+        'exp':datetime.datetime.utcnow() + datetime.timedelta(days=int(os.getenv("REFRESH_TOKEN_EXPIRES_DAYS",7))),
+        'iat':datetime.datetime.utcnow()
     }
-    return jwt.encode(payload, os.getenv("JWT_REFRESH_KEY"), algorithm='HS256')
+    token = jwt.encode(payload,os.getenv("JWT_REFRESH_KEY"),algorithm='HS256')
+    return token
 
-
-def decoded_token(token, is_refresh=False):
+def decoded_token(token,is_refresh=False):
     secret = os.getenv("JWT_REFRESH_KEY") if is_refresh else os.getenv("JWT_KEY")
     try:
-        return jwt.decode(token, secret, algorithms=['HS256'])
-    except jwt.ExpiredSignatureError:
-        return {"error": "expired"}
-    except jwt.InvalidTokenError:
-        return {"error": "invalid"}
+        payload = jwt.decode(token, secret, algorithms=['HS256'])
+        return payload
+    except jwt.ExpiredSignatureError as e:
+        return None
+    except jwt.InvalidTokenError as e:
+        return None
 
         
