e5570b1632afe040aed16949382d0245ac6de576
emmanuel amankwah <emmanuelamankwah004@gmail.com>
2026-06-21 18:49:19 +0000

    change in database columns

--- backend/repository/otp_repository.py
+++ backend/repository/otp_repository.py
@@ -43,8 +43,8 @@ def get_latest_otp(email: str):
     try:
         cursor.execute(
             """
-            SELECT id, otp_hash, expires_at, used, created_at
-            FROM email_otps
+            SELECT id, otp_hash, expiry, used, created_at
+            FROM otp_table
             WHERE email = %s
             ORDER BY created_at DESC
             LIMIT 1
